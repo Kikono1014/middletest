05bf8d17a82e24ca3111aa984c97f3d957f46201
Kikono1014 <vitalik.pyvovarchuk@gmail.com>
2025-03-28 13:08:11 +0200

    added fixture for making temporary file. updated test_red_file

--- test_main.py
+++ test_main.py
@@ -2,10 +2,15 @@ import pytest
 import main
 
 
-def test_read_file(tmp_path):
-    file_content = ["line1\n", "line2\n", "line3\n"]
-    file_path = tmp_path / "test.txt"
-    file_path.write_text("".join(file_content))
-    
+@pytest.fixture
+def sample_file(tmp_path):
+    content = ["line1\n", "line2\n", "line3\n"]
+    file_path = tmp_path / "sample.txt"
+    file_path.write_text("".join(content))
+    return file_path, content
+
+def test_read_file(sample_file):
+    file_path, content = sample_file
     result = main.read_file(str(file_path))
-    assert result == file_content
+    assert result == content
+
