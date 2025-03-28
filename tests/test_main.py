618eb92705865a18a466b1265bd9ee570e9739d9
Kikono1014 <vitalik.pyvovarchuk@gmail.com>
2025-03-28 13:01:03 +0200

    added test for read_file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,11 @@
 import pytest
 import main
 
+
+def test_read_file(tmp_path):
+    file_content = ["line1\n", "line2\n", "line3\n"]
+    file_path = tmp_path / "test.txt"
+    file_path.write_text("".join(file_content))
+    
+    result = main.read_file(str(file_path))
+    assert result == file_content
